6fbf90ca8fef806bf87b1d332c13b404c00c6ff1
MajedAlSalahi <Majed1AlSalahy7@gmail.com>
2026-04-25 23:02:48 +0300

    3تحديث

--- src/components/ui/PWAInstaller.tsx
+++ src/components/ui/PWAInstaller.tsx
@@ -1,4 +1,4 @@
-﻿'use client';
+'use client';
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import { FaDownload, FaXmark, FaMobileScreen, FaShare } from 'react-icons/fa6';
@@ -152,3 +152,4 @@ export default function PWAInstaller() {
     </AnimatePresence>
   );
 }
+
